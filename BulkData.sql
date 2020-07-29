49f81a432fe644f8de9efeae4c038c452110628b
Keyur012 <keyur216tarsariya@gmail.com>
2020-07-29 12:39:43 +0530

    bulk data added

---
+++ BulkData.sql
@@ -0,0 +1,165 @@
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('cs01','Aarush','aarush20@gmail.com','2001-05-21','385632','Ahemdabad','comps','B.Tech','A','anvsasf');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('cs02','Aayush','aayush20@gmail.com','2001-02-11','385632','Ahemdabad','comps','B.Tech','B','pass');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('cs03','Krish','krtish555@gmail.com','2000-11-17','395632','Surat','comps','B.Tech','B','safdv');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('cs04','Hiran','hiran10@gmail.com','2000-11-25','395632','Baroda','comps','M.Tech','A','dsgfdhrt');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('cs05','Ivan','ivan020@gmail.com','1999-01-08','355632','Surat','comps','M.Tech','A','jgdhfd');
+
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('me01','Mehul','mehul@gmail.com','2001-05-14','395002','Surat','Mech','B.Tech','A','anvkbs');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('me02','Nakul','nakul8@gmail.com','2001-06-20','395002','Surat','Mech','B.Tech','A','a85f');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('me03','Ojas','ojas15@gmail.com','2000-03-07','385632','Ahemdabad','Mech','B.Tech','B','anv4sasf');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('me04','Ryan','ryan7@gmail.com','1999-05-23','385632','Ahemdabad','Mech','M.Tech','A','an78sf');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('me05','Sahil','sahil84@gmail.com','1999-05-21','355632','Baroda','Mech','M.Tech','A','a1100asf');
+
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('ee01','Aarush','aaru0@gmail.com','2001-12-04','385632','Ahemdabad','elctric','B.Tech','A','an84');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('ee02','Hiran','hiran85@gmail.com','2001-05-07','395005','Surat','elctric','B.Tech','A','85sasf');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('ee03','Mehul','mehul@gmail.com','2000-07-21','385632','Ahemdabad','elctric','B.Tech','B','zcvx');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('ee04','Ivan','ivan@gmail.com','1999-08-20','355012','Baroda','elctric','M.Tech','A','qwet');
+insert into student(studentId,studentName,studentEmail,studentDob,studentPincode,studentCity,studentBranch,studentProgramme,studentClass,studentPortalPassword) values('ee05','Tejas','tejass@gmail.com','1999-09-20','385632','Ahemdabad','elctric','M.Tech','A','sdhdb');
+
+
+insert into typesoffee values('A',100000);
+insert into typesoffee values('B',50000);
+insert into typesoffee values('C',25000);
+
+insert into studentphonenumbers values('cs01','8548484898');
+insert into studentphonenumbers values('cs02','8154184898');
+insert into studentphonenumbers values('cs03','9852284898');
+insert into studentphonenumbers values('cs04','7512084898');
+insert into studentphonenumbers values('cs05','7841484898');
+insert into studentphonenumbers values('me01','7458584898');
+insert into studentphonenumbers values('me02','7458584898');
+insert into studentphonenumbers values('me03','8548484855');
+insert into studentphonenumbers values('me04','8548484254');
+insert into studentphonenumbers values('me05','8548481474');
+insert into studentphonenumbers values('me05','8548481788');
+insert into studentphonenumbers values('ee01','8548474898');
+insert into studentphonenumbers values('ee02','8548985958');
+insert into studentphonenumbers values('ee03','8514875688');
+insert into studentphonenumbers values('ee04','8548441147');
+insert into studentphonenumbers values('ee04','9589984898');
+insert into studentphonenumbers values('ee05','9999999999');
+
+insert into studenthasfeetype values('cs01','A');
+insert into studenthasfeetype values('cs02','B');
+insert into studenthasfeetype values('cs03','B');
+insert into studenthasfeetype values('cs04','C');
+insert into studenthasfeetype values('cs05','C');
+insert into studenthasfeetype values('me01','A');
+insert into studenthasfeetype values('me02','B');
+insert into studenthasfeetype values('me03','B');
+insert into studenthasfeetype values('me04','C');
+insert into studenthasfeetype values('me05','C');
+insert into studenthasfeetype values('ee01','A');
+insert into studenthasfeetype values('ee02','B');
+insert into studenthasfeetype values('ee03','B');
+insert into studenthasfeetype values('ee04','C');
+insert into studenthasfeetype values('ee05','C');
+
+insert into hostels values(61,'Gajjar Bhavan');
+insert into hostels values(71,'Bhabha Bhavan');
+
+insert into studenthostel values('cs01','h01',61,201);
+insert into studenthostel values('cs03','h02',61,202);
+insert into studenthostel values('cs05','h03',71,203);
+insert into studenthostel values('me01','h04',61,204);
+insert into studenthostel values('me02','h05',61,205);
+insert into studenthostel values('me03','h06',61,206);
+insert into studenthostel values('me04','h07',71,207);
+insert into studenthostel values('ee03','h08',61,208);
+insert into studenthostel values('ee04','h09',71,209);
+insert into studenthostel values('ee05','h010',71,210);
+
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em01','Mp patel','mp@gmail.com','2005-01-25','M.Tech',395100,'Surat','fdgg',0);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em02','Lm Gandhi','lm@gmail.com','2006-10-25','M.Tech',395100,'Surat','ffsddfkh',0);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em03','o Kohli','kohli@gmail.com','2008-08-11','M.Tech',395100,'Surat','sfk12h',0);
+
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em04','ty patel','ty@gmail.com','2005-06-15','Phd',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em05','jd Gandhi','jd@gmail.com','2012-01-26','M.Tech',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em06','dh Kohli','dh@gmail.com','2014-01-05','M.Tech',395100,'Surat','fdskh',1);
+
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em07','qp patel','qp@gmail.com','2005-01-25','Phd',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em08','gp Gandhi','gp@gmail.com','2011-01-25','M.Tech',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em09','Mm Kohli','mm@gmail.com','2012-01-25','M.Tech',395100,'Surat','fdskh',1);
+
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em10','jh patel','jh@gmail.com','2005-01-25','Phd',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em11','sh Gandhi','sh@gmail.com','2015-01-25','M.Tech',395100,'Surat','fdskh',1);
+insert into employee(employeeId,employeeName,employeeEmail,employeeJoiningDate,employeeHighestDegree,employeePincode,employeeCity,employeePortalPassword,isTeacher) values ('Em12','th Kohli','th@gmail.com','2016-01-25','M.Tech',395100,'Surat','fdskh',1);
+
+
+insert into employeephonenumbers values('Em01','8496968574');
+insert into employeephonenumbers values('Em02','8874968574');
+insert into employeephonenumbers values('Em03','8496254574');
+insert into employeephonenumbers values('Em04','8496958774');
+insert into employeephonenumbers values('Em07','8499858574');
+insert into employeephonenumbers values('Em10','8497458574');
+insert into employeephonenumbers values('Em01','9854748574');
+
+insert into admin values ('Em01',251,'TW','Director');
+insert into admin values ('Em02',281,'RY','Registar');
+insert into admin values ('Em03',111,'AC','Dean');
+
+insert into departmentnames values('d1','computer');
+insert into departmentnames values('d2','Mechanical');
+insert into departmentnames values('d3','Electric');
+
+insert into teacher values('Em04','d1','HOD');
+insert into teacher values('Em05','d1','professor');
+insert into teacher values('Em06','d1','Teacher');
+
+insert into teacher values('Em07','d2','HOD');
+insert into teacher values('Em08','d2','professor');
+insert into teacher values('Em09','d2','Teacher');
+
+insert into teacher values('Em10','d3','HOD');
+insert into teacher values('Em11','d3','professor');
+insert into teacher values('Em12','d3','Teacher');
+
+insert into projects values('Em04','Machine Learning');
+insert into projects values('Em04','Algorithm');
+
+insert into projects values('Em07','Less fuel ');
+insert into projects values('Em07','Boiler');
+
+insert into projects values('Em10','Electric Vehicle');
+
+insert into expertise values('Em04','Machine Learning');
+insert into expertise values('Em07','Power');
+insert into expertise values('Em10','Electric');
+
+insert into supervisor values('Em04','Em06');
+insert into supervisor values('Em04','Em05');
+insert into supervisor values('Em07','Em09');
+
+insert into courses values('S01','Em04','Automata',2020);
+insert into courses values('S02','Em07','Mechanical',2020);
+insert into courses values('S03','Em10','Power',2020);
+
+insert into studentEnroll values('cs01','S01',2020,51);
+insert into studentEnroll values('cs02','S01',2020,50);
+insert into studentEnroll values('cs03','S01',2020,75);
+insert into studentEnroll values('cs04','S01',2020,85);
+insert into studentEnroll values('cs05','S01',2020,25);
+
+insert into studentEnroll values('me01','S02',2020,78);
+insert into studentEnroll values('me02','S02',2020,82);
+insert into studentEnroll values('me03','S02',2020,53);
+insert into studentEnroll values('me04','S02',2020,25);
+insert into studentEnroll values('me05','S02',2020,57);
+
+insert into studentEnroll values('ee01','S03',2020,63);
+insert into studentEnroll values('ee02','S03',2020,85);
+insert into studentEnroll values('ee03','S03',2020,91);
+insert into studentEnroll values('ee04','S03',2020,24);
+insert into studentEnroll values('ee05','S03',2020,15);
+
+insert into taugthby(teacherId,courseId) values('Em04','S01'); 
+insert into taugthby(teacherId,courseId) values('Em05','S01');
+insert into taugthby(teacherId,courseId) values('Em06','S01');
+
+insert into taugthby(teacherId,courseId) values('Em07','S02'); 
+insert into taugthby(teacherId,courseId) values('Em08','S02');
+insert into taugthby(teacherId,courseId) values('Em09','S02');
+
+insert into taugthby(teacherId,courseId) values('Em10','S03'); 
+insert into taugthby(teacherId,courseId) values('Em11','S03');
+insert into taugthby(teacherId,courseId) values('Em12','S03');
