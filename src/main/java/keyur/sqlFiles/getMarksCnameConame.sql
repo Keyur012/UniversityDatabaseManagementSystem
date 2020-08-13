007ec6a16de171a89e376393e09bb39460c16da3
Keyur012 <44499857+Keyur012@users.noreply.github.com>
2020-08-13 15:38:36 +0530

    Update getMarksCnameConame.sql

--- src/main/java/keyur/sqlFiles/getMarksCnameConame.sql
+++ src/main/java/keyur/sqlFiles/getMarksCnameConame.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS finalp.getMarksCnameConame;
 
 Delimiter $$
 
-CREATE PROCEDURE `getMarksCnameConame`(In stu varchar(10),in cou varchar(10),out marks int,cname varchar(20),out coname varchar(10))
+CREATE PROCEDURE `getMarksCnameConame`(In stu varchar(10),in cou varchar(10),out marks int,out cname varchar(20),out coname varchar(10))
 BEGIN
 	select studentenroll.marks,courses.courseName,employee.employeename  into marks,cname,coname from (((student inner join studentenroll on student.studentId = studentenroll.studentId ) inner join courses on studentenroll.courseId = courses.courseId ) inner join employee on courses.courseCoordinatorId = employee.employeeId) where student.studentId = stu and courses.sourseId = cou;
 END $$
